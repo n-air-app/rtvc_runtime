8508840f00cfbd6c6b764522f97b5034f37427af
asaday <asada@nagisaworks.com>
2024-03-29 18:00:33 +0900

    fix ps1

--- make_archive.ps1
+++ make_archive.ps1
@@ -2,7 +2,7 @@
 Remove-Item build -Recurse
 New-Item build -ItemType Directory
 Set-Location build
-Copy-Item ../bin/* .
-Copy-Item ../thirdparty/onemkl/*.dll .
+Copy-Item ../bin/* . -Recurse
+Copy-Item ../thirdparty/onemkl/*.dll .  -Recurse
 tar -czvf ../nair-rtvc.tar.gz .
 Set-Location ..
